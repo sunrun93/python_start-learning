21c4af7bb47ba2529ca5dfc3b2bf823a4be88ee6
sunrun93 <wangjian9302@163.com>
2018-02-12 17:17:46 +0800

    fit function

--- 高级特性/generator.py
+++ 高级特性/generator.py
@@ -8,3 +8,20 @@ g = (x * x for x in range(10));
 print(g); #<generator object <genexpr> at 0x000002086AEC6ED0>
 
 print(next(g));
+
+#著名的斐波拉契数列（Fibonacci），除第一个和第二个数外，任意一个数都可由前两个数相加得到：
+#1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+
+def fib(max):
+    n,a,b=0,0,1
+    while n <max:
+        print(b)
+        a,b=b,a+b
+        n = n+1
+    return 'done'
+    
+fib(6);
+
+    #t = (b, a + b) # t是一个tuple
+    #a = t[0]
+    #b = t[1]
