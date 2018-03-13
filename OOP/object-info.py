40b26137994893563ea9c4a8757ec37936a188a1
sunrun93 <wangjian9302@163.com>
2018-03-13 09:03:11 +0800

    add object info file

--- OOP/object-info.py
+++ OOP/object-info.py
@@ -21,3 +21,35 @@ print(
      type(lambda x: x), #<class 'function'>
      type((x for x in range(10))) #<class 'generator'>
 ); 
+
+#isinstance() class的继承关系来说，使用type()就很不方便。我们要判断class的类型，可以使用isinstance()函数
+
+class Animal(object):
+    def run(self):
+        print('Animal is running...');
+
+class Dog(Animal):
+    pass;
+
+class Cat(Animal):
+    pass;
+
+a = Animal();
+d = Dog();
+print(isinstance(a,Animal)); #True
+print(isinstance(d,Dog)); #True
+print(isinstance(d,Animal)); #True
+print(isinstance(d,Dog) and isinstance(d,Animal)); #True
+
+#并且还可以判断一个变量是否是某些类型中的一种，比如下面的代码就可以判断是否是list或者tuple：
+print(isinstance([1, 2, 3], (list, tuple))); #True
+print(isinstance((1, 2, 3), (list, tuple))); #True
+
+#dir()  获得一个对象的所有属性和方法
+print(dir('ABC'));
+
+#类似__xxx__的属性和方法在Python中都是有特殊用途的，比如__len__方法返回长度
+print('ABC'.__len__()); #3
+print(len('ABC')); #3
+
+#把属性和方法列出来是不够的，配合getattr()、setattr()以及hasattr()，我们可以直接操作一个对象的状态
