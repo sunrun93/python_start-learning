af29125852abf6ba6d5ec85d93eb85ab4419dcf6
sunrun93 <wangjian9302@163.com>
2018-03-19 21:13:51 +0800

    update customized class

--- OOP2/customized_class.py
+++ OOP2/customized_class.py
@@ -24,6 +24,60 @@ for n in Fib():
      print(n)
 
 #__getitem__ 根据下标从类中取出元素
+class Fib1(object):
+    def __getitem__(self, n):
+        a, b = 1, 1
+        for x in range(n):
+            a, b = b, a + b
+        return a
+f2 = Fib1();
+# 按下标访问数列的任意一项
+print(f2[0],f2[1],f2[2]);# 1  1  2
+
+#__getattr__ 写一个__getattr__()方法，动态返回一个属性
+class Student1(object):
+
+    def __init__(self):
+        self.name = 'Michael'
+s1 = Student1();
+# print(s1.score);#AttributeError: 'Student1' object has no attribute 'score
+
+#写一个__getattr__()方法，动态返回一个属性,即使不存在的属性，也能避免报错
+class Student(object):
+
+    def __init__(self):
+        self.name = 'Michael'
+
+    def __getattr__(self, attr):
+        if attr=='score':
+            return 99
+        if attr=='age': #可以返回一个函数
+            return lambda: 25
+
+s=Student();
+print(s.score,s.age());#99
+print(s.gender); #None
+
+#__call__()方法，定义直接在实例本身上调用
+class Student2(object):
+    def __init__(self, name):
+        self.name = name
+    def __call__(self):
+        print('My name is %s.' % self.name)
+
+s2 = Student2('Nancy');
+s2(); #My name is Nancy.通过对象本身即可调用
+
+#判断一个对象是否能被调用，能被调用的对象就是一个Callable对象
+print(
+    callable(Student()), #false
+    callable(max), #true
+    callable([1, 2, 3]), #false
+    callable(Student2('Nancy')), #true
+    callable(None) #false
+    );
+
+
 
 
 
